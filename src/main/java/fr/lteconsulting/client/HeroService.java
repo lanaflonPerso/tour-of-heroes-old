aa5e53c0d8cd8212f3b98c853cda9a570fefeb7b
Arnaud Tournier <ltearno@gmail.com>
2016-07-14 17:54:29 +0200

    step 18 - Getting Heroes

--- src/main/java/fr/lteconsulting/client/HeroService.java
+++ src/main/java/fr/lteconsulting/client/HeroService.java
@@ -1,10 +1,15 @@
 package fr.lteconsulting.client;
 
 import fr.lteconsulting.angular2gwt.Injectable;
+import fr.lteconsulting.angular2gwt.client.JsArray;
 import jsinterop.annotations.JsType;
 
 @Injectable
 @JsType
 public class HeroService
 {
+	public JsArray<Hero> getHeroes()
+	{
+		return null;
+	}
 }
