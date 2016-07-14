7c69fd602c11016613e43d815231e3d33bc3d4a5
Arnaud Tournier <ltearno@gmail.com>
2016-07-14 17:11:52 +0200

    step 8 - Listing Heroes with ngFor

--- src/main/java/fr/lteconsulting/client/AppComponent.java
+++ src/main/java/fr/lteconsulting/client/AppComponent.java
@@ -8,6 +8,12 @@ import jsinterop.annotations.JsType;
 @Component(
 		selector = "my-app",
 		template = "<h1>{{title}}</h1>" +
+				"<h2>My Heroes</h2>" +
+				"<ul class='heroes'>" +
+				"<li *ngFor='let hero of heroes'>" +
+				"<span class='badge'>{{hero.id}}</span> {{hero.name}}" +
+				"</li>" +
+				"</ul>" +
 				"<h2>{{hero.name}} details!</h2>" +
 				"<div><label>id: </label>{{hero.id}}</div>" +
 				"<div>" +
