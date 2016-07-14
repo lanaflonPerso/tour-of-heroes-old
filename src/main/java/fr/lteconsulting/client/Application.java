41179c45b4e4fc134222669b614a71946a40df18
Arnaud Tournier <ltearno@gmail.com>
2016-07-14 17:07:08 +0200

    step 6 - Creating Heroes

--- src/main/java/fr/lteconsulting/client/Application.java
+++ src/main/java/fr/lteconsulting/client/Application.java
@@ -2,6 +2,7 @@ package fr.lteconsulting.client;
 
 import com.google.gwt.core.client.EntryPoint;
 
+import fr.lteconsulting.angular2gwt.client.JsArray;
 import fr.lteconsulting.angular2gwt.client.interop.angular.Angular;
 
 /**
@@ -9,6 +10,18 @@ import fr.lteconsulting.angular2gwt.client.interop.angular.Angular;
  */
 public class Application implements EntryPoint
 {
+	public static JsArray<Hero> HEROES = JsArray.of(
+			new Hero( 11, "Mr. Nice" ),
+			new Hero( 12, "Narco" ),
+			new Hero( 13, "Bombasto" ),
+			new Hero( 14, "Celeritas" ),
+			new Hero( 15, "Magneta" ),
+			new Hero( 16, "RubberMan" ),
+			new Hero( 17, "Dynama" ),
+			new Hero( 18, "Dr IQ" ),
+			new Hero( 19, "Magma" ),
+			new Hero( 20, "Tornado" ) );
+
 	@Override
 	public void onModuleLoad()
 	{
