98bc743d9a170a2e2c9efb5ebf2f369bd25b8608
Arnaud Tournier <ltearno@gmail.com>
2016-08-02 09:00:33 +0200

    step 28 - Add a Dashboard

---
+++ src/main/java/fr/lteconsulting/client/DashboardComponent.java
@@ -0,0 +1,13 @@
+package fr.lteconsulting.client;
+
+import fr.lteconsulting.angular2gwt.Component;
+import jsinterop.annotations.JsType;
+
+@Component(
+		selector = "my-dashboard",
+		template = "<h3>My Dashboard</h3>" )
+@JsType
+public class DashboardComponent
+{
+
+}
