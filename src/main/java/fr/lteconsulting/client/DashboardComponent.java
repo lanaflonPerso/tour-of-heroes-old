2e2f029ff3812389423a49e0788ea7598a0d206f
Arnaud Tournier <ltearno@gmail.com>
2016-08-02 09:49:05 +0200

    step 30 - Dashboard Top Heroes

--- src/main/java/fr/lteconsulting/client/DashboardComponent.java
+++ src/main/java/fr/lteconsulting/client/DashboardComponent.java
@@ -1,13 +1,39 @@
 package fr.lteconsulting.client;
 
 import fr.lteconsulting.angular2gwt.Component;
+import fr.lteconsulting.angular2gwt.client.JsArray;
+import fr.lteconsulting.angular2gwt.client.interop.angular.NgZone;
+import fr.lteconsulting.angular2gwt.client.interop.angular.OnInit;
+import jsinterop.annotations.JsMethod;
+import jsinterop.annotations.JsProperty;
 import jsinterop.annotations.JsType;
 
 @Component(
 		selector = "my-dashboard",
-		template = "<h3>My Dashboard</h3>" )
+		templateUrl = "dashboard.component.html" )
 @JsType
-public class DashboardComponent
+public class DashboardComponent implements OnInit
 {
+	private HeroService heroService;
 
+	@JsProperty
+	private JsArray<Hero> heroes;
+
+	public DashboardComponent( HeroService heroService, NgZone zone )
+	{
+		this.heroService = heroService;
+	}
+
+	@Override
+	public void ngOnInit()
+	{
+		heroService.getHeroes().then( ( heroes ) -> {
+			this.heroes = heroes.slice( 1, 5 );
+		}, null );
+	}
+
+	@JsMethod
+	private void gotoDetail()
+	{
+	}
 }
